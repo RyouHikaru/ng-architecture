7dcaa5f5631642739edb7ba28a024c2e492d9690
RyouHikaru <carljamesreblora13@gmail.com>
2023-02-23 12:58:15 +0800

    Added Counter reducer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
-import { HttpClientModule } from "@angular/common/http";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { NgSharedModule } from 'ng-shared';
 import { StoreModule } from '@ngrx/store';
 import { MyCounterComponent } from './my-counter/my-counter.component';
+import { counterReducer } from './counter.reducer';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodosComponent,
-    MyCounterComponent
-  ],
+  declarations: [AppComponent, TodosComponent, MyCounterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,9 +21,9 @@ import { MyCounterComponent } from './my-counter/my-counter.component';
     BrowserAnimationsModule,
     MaterialModule,
     NgSharedModule,
-    StoreModule.forRoot({})
+    StoreModule.forRoot({ count: counterReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { increment, decrement, reset } from '../counter.actions';
 
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
-  styleUrls: ['./my-counter.component.scss']
+  styleUrls: ['./my-counter.component.scss'],
 })
 export class MyCounterComponent {
   count$: Observable<number>;
 
-  constructor() {
-    this.count$ = new Observable<number>;
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.pipe(select('count'));
   }
 
-  increment() {}
-  decrement() {}
-  reset() {}
-
+  increment() {
+    this.store.dispatch(increment());
+  }
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
